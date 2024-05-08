41ad9658c4c1b07966ec694a360c0c9741008a70
User Name <dana@gmail.com>
2024-05-08 15:08:56 +0300

    first

---
+++ WpfApp2/WpfApp2/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        systemtestEntities1 db = new systemtestEntities1();
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            Admin1 admin1 = new Admin1();
+            admin1 = db.Admin1.FirstOrDefault(x=> x.User_Name == textboxU.Text);
+            if (admin1 != null && admin1.password1 == passU.Password)
+            {
+                Page2 page2 = new Page2();
+                this.NavigationService.Navigate(page2);
+            }
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/Page2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        systemtestEntities1 db = new systemtestEntities1();
+        public Page2()
+        {
+            InitializeComponent();
+             
+        }
+
+        private void search1_Click(object sender, RoutedEventArgs e)
+        {
+            employee employee = new employee();
+            int v = int.Parse(idt.Text);
+            datagrid.ItemsSource = db.employees.Where(x => x.id == v).ToList();
+        }
+    }
+}
